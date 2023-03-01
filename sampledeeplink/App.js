5217e3c20f599ab0659dfe23ccef8bafcba70187
Vineel <vinilk8@gmail.com>
2023-03-01 17:18:55 +0530

    firebase changes added

--- sampledeeplink/App.js
+++ sampledeeplink/App.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View, Text} from 'react-native';
+import dynamicLinks from '@react-native-firebase/dynamic-links';
 
 const App = () => {
+  const handleDynamicLink = link => {
+    console.log('Foreground link ', link);
+  };
+
+  useEffect(() => {
+    const unsubscribe = dynamicLinks().onLink(handleDynamicLink);
+    return () => unsubscribe();
+  }, []);
+
+  useEffect(() => {
+    dynamicLinks()
+      .getInitialLink()
+      .then(link => {
+        console.log('Background link ', link);
+      });
+  }, []);
+
   return (
     <View
       style={{
